e7f64e7747eab6ab8b602a491ed88c99851a6735
jackyangantelope <jack@retroremake.co>
2025-12-03 16:56:26 +0800

    modify frame

--- Arcade-CosmicGuerilla.sv
+++ Arcade-CosmicGuerilla.sv
@@ -63,6 +63,7 @@ module emu
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 	output        HDMI_BLACKOUT,
+	output        HDMI_BOB_DEINT,
 
 `ifdef MISTER_FB
 	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
@@ -195,6 +196,7 @@ assign VGA_SCALER = 0;
 assign VGA_DISABLE = 0;
 assign HDMI_FREEZE = 0;
 assign HDMI_BLACKOUT = 0;
+assign HDMI_BOB_DEINT = 0;
 assign FB_FORCE_BLANK = 0;
 
 assign LED_USER  = 0;
